b005330a0bfe20ba7d7d91192a3d1ac4c025c1dd
Ravi Punj <ravi.punj@gmail.com>
2018-05-24 13:25:19 -0400

    Added TimeReportEntry model

--- models.py
+++ models.py
@@ -10,3 +10,38 @@ class TimeReport(db.Model):
 
     def __repr__(self):
         return "[TimeReport id={id}]".format(id=self.id)
+
+    def __eq__(self, other):
+        return self.id == other.id
+
+
+class TimeReportEntry(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now)
+    modified = db.Column(db.DateTime, nullable=False,
+                         default=datetime.datetime.now, onupdate=datetime.datetime.now)
+    report_id = db.Column(db.Integer, db.ForeignKey("time_report.id"))
+    employee_id = db.Column(db.Integer, nullable=False)
+    date = db.Column(db.Date, nullable=False, index=True)
+    hours_worked = db.Column(db.Integer, nullable=False)
+    job_group = db.Column(db.String(1), nullable=False)
+
+    def __repr__(self):
+        return "[TimeReportEntry employee_id={employee_id} " \
+                                "date={date} " \
+                                "hours_worked={hours_worked} " \
+                                "job_group={job_group}]".format(
+                                    employee_id=self.employee_id,
+                                    date=self.date,
+                                    hours_worked=self.hours_worked,
+                                    job_group=self.job_group,
+                                )
+
+    def __eq__(self, other):
+        return (
+            self.report_id == other.report_id and
+            self.employee_id == other.employee_id and
+            self.date == other.date and
+            self.hours_worked == other.hours_worked and
+            self.job_group == other.job_group
+        )
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,7 +1,10 @@
+import datetime
+
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm.exc import FlushError
 
 from app import db
-from models import TimeReport
+from models import TimeReport, TimeReportEntry
 from tests.base import BaseTestCase
 
 def insert_time_report(**overrides):
@@ -15,6 +18,25 @@ def insert_time_report(**overrides):
 
     return time_report
 
+def insert_time_report_entry(**overrides):
+    time_report = \
+        overrides.pop("time_report") if "time_report" in overrides else insert_time_report()
+    time_report_entry_params = {
+        "report_id": time_report.id,
+        "employee_id": 99,
+        "date": datetime.date(2010, 4, 23),
+        "hours_worked": 7,
+        "job_group": "A"
+    }
+    time_report_entry_params.update(overrides)
+
+    time_report_entry = TimeReportEntry(**time_report_entry_params)
+
+    db.session.add(time_report_entry)
+    db.session.commit()
+
+    return time_report_entry
+
 
 class TestTimeReport(BaseTestCase):
     def test_repr(self):
@@ -23,9 +45,42 @@ class TestTimeReport(BaseTestCase):
 
 
 class TestTimeReportInsertion(BaseTestCase):
-    def test_raises_integrity_error_for_missing_id_field(self):
+    def test_raises_error_for_missing_id_field(self):
         self.assertRaises(FlushError, insert_time_report, id=None)
 
-    def test_insert_time_report(self):
+    def test_inserts_time_report(self):
         time_report = insert_time_report()
         self.assertEqual(db.session.query(TimeReport).one(), time_report)
+
+
+class TestTimeReportEntry(BaseTestCase):
+    def test_repr(self):
+        time_report_entry = TimeReportEntry(
+            report_id=1,
+            employee_id=1,
+            hours_worked=3,
+            date=datetime.date(2014, 5, 6),
+            job_group="A",
+        )
+        self.assertEqual(
+            str(time_report_entry),
+            "[TimeReportEntry employee_id=1 date=2014-05-06 hours_worked=3 job_group=A]"
+        )
+
+
+class TestTimeReportEntryInsertion(BaseTestCase):
+    def test_raises_error_for_missing_employee_id(self):
+        self.assertRaises(IntegrityError, insert_time_report_entry, employee_id=None)
+
+    def test_raises_error_for_missing_date(self):
+        self.assertRaises(IntegrityError, insert_time_report_entry, date=None)
+
+    def test_raises_error_for_missing_hours_worked(self):
+        self.assertRaises(IntegrityError, insert_time_report_entry, hours_worked=None)
+
+    def test_raises_error_for_missing_job_group(self):
+        self.assertRaises(IntegrityError, insert_time_report_entry, job_group=None)
+
+    def test_inserts_time_report_entry(self):
+        time_report_entry = insert_time_report_entry()
+        self.assertEqual(db.session.query(TimeReportEntry).one(), time_report_entry)
