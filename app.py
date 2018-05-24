b2c2cb1efadf172901dbcbbd8a900da1160f3a09
Ravi Punj <ravi.punj@gmail.com>
2018-05-24 04:03:53 -0400

    Added Flask_SQLAlchemy and Flask_Migrate to deal with database ORM and migrations

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask_api import FlaskAPI
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = FlaskAPI(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/payroll.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+db = SQLAlchemy(app=app)
+migrate = Migrate(app=app, db=db)
 
 @app.route('/health', methods=['GET'])
 def health():
